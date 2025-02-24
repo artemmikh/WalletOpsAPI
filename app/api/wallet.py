73d4edc45352a293adfba7b8e456e3fae0322fdd
Михайлов Артём <artem.mikh100@gmail.com>
2025-02-24 15:11:37 +0300

    Add get wallet balance endpoint

--- app/api/wallet.py
+++ app/api/wallet.py
@@ -1,8 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.db import get_async_session
+from app.crud.wallet import wallet_crud
+from app.schemas.wallet import WalletDB
 
 router = APIRouter()
 
 
-@router.get('/{WALLET_UUID}')
-async def test_app(WALLET_UUID):
-    return WALLET_UUID
+@router.get(
+    '/{WALLET_UUID}',
+    response_model=WalletDB
+)
+async def get_wallet_balance(
+        WALLET_UUID,
+        session: AsyncSession = Depends(get_async_session)
+):
+    return await wallet_crud.get_by_uuid(WALLET_UUID, session)
