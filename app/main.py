e3a6d45a8256d3151a2c6a1a0a9969aa5d865882
Михайлов Артём <artem.mikh100@gmail.com>
2025-02-24 12:29:14 +0300

    Add auto create tables on startup

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 
 from app.api.wallet import router as wallet_router
 from app.core.config import settings
+from app.core.db import engine, Base
+from app.models.wallet import Wallet  # noqa
 
 app = FastAPI(
     title=settings.app_title,
@@ -15,3 +17,11 @@ main_router.include_router(
     tags=['Wallet Operations'])
 
 app.include_router(main_router)
+
+
+@app.on_event("startup")
+async def on_startup():
+    # временная функция до создания механизама миграций
+    """Создание таблиц при старте приложения."""
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
