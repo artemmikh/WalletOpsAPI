7b429fa1a2f905edc39d03ae61fb6b6c09c55fbf
Михайлов Артём <artem.mikh100@gmail.com>
2025-02-25 13:05:22 +0300

    Add WalletOperation schema. Add examples

--- app/schemas/wallet.py
+++ app/schemas/wallet.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from pydantic import BaseModel
 
 
@@ -8,3 +10,35 @@ class WalletDB(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class OperationType(str, Enum):
+    DEPOSIT = 'DEPOSIT'
+    WITHDRAW = 'WITHDRAW'
+
+
+class WalletOperation(BaseModel):
+    operationType: OperationType
+    amount: float
+
+    class Config:
+        schema_extra = {
+            'examples': {
+                'deposit': {
+                    'summary': 'Пополнение баланса',
+                    'description': 'Операция пополнения кошелька.',
+                    'value': {
+                        'operationType': 'DEPOSIT',
+                        'amount': 1000
+                    }
+                },
+                'withdraw': {
+                    'summary': 'Снятие средств',
+                    'description': 'Операция списания средств с кошелька.',
+                    'value': {
+                        'operationType': 'WITHDRAW',
+                        'amount': 500
+                    }
+                }
+            }
+        }
