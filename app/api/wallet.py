f448a8d4dcef4361e936c12d73531fb802ffa7df
Михайлов Артём <artem.mikh100@gmail.com>
2025-02-25 13:05:52 +0300

    Add change_wallet_balance endpoint

--- app/api/wallet.py
+++ app/api/wallet.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Body
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.validators import check_wallet_exists
 from app.core.db import get_async_session
-from app.schemas.wallet import WalletDB
+from app.schemas.wallet import WalletDB, WalletOperation
 
 router = APIRouter()
 
@@ -18,3 +18,17 @@ async def get_wallet_balance(
 ):
     wallet = await check_wallet_exists(wallet_uuid, session)
     return wallet
+
+
+@router.post(
+    '/{WALLET_UUID}/operation',
+    # response_model=WalletDB
+)
+async def change_wallet_balance(
+        wallet_uuid,
+        operation: WalletOperation = Body(
+            ..., examples=WalletOperation.Config.schema_extra['examples']
+        ),
+        session: AsyncSession = Depends(get_async_session)
+):
+    return 'ok'
