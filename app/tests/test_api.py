e32b54997c8304aebebf0eb956cc98403b23c710
Михайлов Артём <artem.mikh100@gmail.com>
2025-02-26 14:59:01 +0300

    Add test_wallet balance not found

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -11,10 +11,25 @@ async def test_wallet_balance(
         test_wallet,
         valid_wallet: Wallet
 ):
+    """Тест на получение баланса существующего кошелька."""
     response = await client.get(f'{api_url}/{valid_wallet.uuid}')
     assert response.status_code == 200
     assert response.json() == {
         "id": valid_wallet.id,
         "uuid": valid_wallet.uuid,
         "balance": valid_wallet.balance
+    }
+
+
+@pytest.mark.asyncio
+async def test_wallet_balance_not_found(
+        api_url: str,
+        client: AsyncClient
+):
+    """Тест на получение баланса несуществующего кошелька."""
+    non_existent_uuid = "00000000-0000-0000-0000-000000000000"
+    response = await client.get(f'{api_url}/{non_existent_uuid}')
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Кошелёк не найден!"
     }
