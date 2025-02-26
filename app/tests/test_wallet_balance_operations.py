8d074c246ac475a19c34812dd2820d5fcbdbc0f2
Михайлов Артём <artem.mikh100@gmail.com>
2025-02-26 15:49:25 +0300

    Add test withdraw wallet not found

--- app/tests/test_wallet_balance_operations.py
+++ app/tests/test_wallet_balance_operations.py
@@ -27,3 +27,24 @@ async def test_withdraw_wallet(
         "uuid": valid_wallet.uuid,
         "balance": valid_wallet.balance - 1
     }
+
+
+@pytest.mark.asyncio
+async def test_withdraw_wallet_not_found(
+        api_url: str,
+        client: AsyncClient,
+        test_wallet,
+        valid_wallet: Wallet
+):
+    """Тест на снятие средств не существующего кошелька."""
+    response = await client.post(
+        f'{api_url}/{"00000000-0000-0000-0000-000000000000"}/operation',
+        json={
+            "operationType": "WITHDRAW",
+            "amount": 1
+        }
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {
+        "detail": "Кошелёк не найден!"
+    }
