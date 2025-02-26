6298bc4c0d92f58ddd2474be111b74945a24046f
Михайлов Артём <artem.mikh100@gmail.com>
2025-02-26 14:43:09 +0300

    Add test wallet balance

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -1,8 +1,20 @@
 import pytest
 from httpx import AsyncClient
 
+from app.models.wallet import Wallet
+
 
 @pytest.mark.asyncio
-async def test_wallet_balance(api_url, client: AsyncClient):
-    response = await client.get(f'/{api_url}/wallets/1')
+async def test_wallet_balance(
+        api_url: str,
+        client: AsyncClient,
+        test_wallet,
+        valid_wallet: Wallet
+):
+    response = await client.get(f'{api_url}/{valid_wallet.uuid}')
     assert response.status_code == 200
+    assert response.json() == {
+        "id": valid_wallet.id,
+        "uuid": valid_wallet.uuid,
+        "balance": valid_wallet.balance
+    }
