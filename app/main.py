fd82e36133363f77783cea01e5efe33e4ea6603b
Михайлов Артём <artem.mikh100@gmail.com>
2025-02-27 14:30:36 +0300

    Del create tables on startup

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ from fastapi import FastAPI
 
 from app.api.wallet import router as wallet_router
 from app.core.config import settings
-from app.core.db import engine, Base
-from app.models.wallet import Wallet  # noqa
 
 app = FastAPI(
     title=settings.app_title,
@@ -18,15 +16,6 @@ main_router.include_router(
     tags=['Wallet Operations'])
 
 app.include_router(main_router)
-
-
-@app.on_event("startup")
-async def on_startup():
-    # временная функция до создания механизма миграций
-    """Создание таблиц при старте приложения."""
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
 
 if __name__ == '__main__':
     # для запуска приложения в debug режиме
