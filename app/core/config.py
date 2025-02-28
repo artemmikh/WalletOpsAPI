6d869f13251a182ec6567cab83da9f835003fa67
Михайлов Артём <artem.mikh100@gmail.com>
2025-02-28 16:24:32 +0300

    Add postgresql config on app settings

--- app/core/config.py
+++ app/core/config.py
@@ -12,9 +12,19 @@ class Settings(BaseSettings):
                             'балансами виртуальных кошельков.')
     database_url: str = 'sqlite+aiosqlite:///./app/data/wallet.db'
     test_database_url: str = 'sqlite+aiosqlite:///:memory:'
-    postgres_user = 'example'
-    postgres_password = 'example'
-    postgres_db = 'example'
+    postgres_user: str = 'example'
+    postgres_password: str = 'example'
+    postgres_db: str = 'example'
+    postgres_host: str = 'db'
+    postgres_port: str = '5432'
+
+    @property
+    def database_url(self) -> str:
+        """Формирует URL для подключения к PostgreSQL."""
+        return (
+            f'postgresql+asyncpg://'
+            f'{self.postgres_user}:{self.postgres_password}@'
+            f'{self.postgres_host}:{self.postgres_port}/{self.postgres_db}')
 
     class Config:
         env_file = '.env'
